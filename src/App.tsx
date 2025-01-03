2cc967fa697649059607c47587966c74ea639ea6
Takacs-Balazs <t.balazs521@gmail.com>
2025-01-03 16:44:49 +0100

    Co-authored-by: KricsDavid <KricsDavid@users.noreply.github.com> Co-authored-by: monusbence <monusbence@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,17 @@ const App = () => {
   return (
     <div>
       <Navbar />
-      <section className="section section1">
-        <h1>Első szekció</h1>
-      </section>
+      <section className="section section1">  
+  <div className="content">
+    <h1>Your party map</h1>
+    <p>Go wild in the city, compete with other partygoers, and enjoy the nightlife wherever you are!</p>
+    <div className="cta-buttons">
+      <a href="#google-play">Get it on Google Play</a>
+      <a href="#app-store">Download on the App Store</a>
+      <a href="#app-gallery">Explore it on AppGallery</a>
+    </div>
+  </div>
+</section>
       <section className="section section2">
         <div className="cards-container">
           <div className="card">Kártya 1</div>
