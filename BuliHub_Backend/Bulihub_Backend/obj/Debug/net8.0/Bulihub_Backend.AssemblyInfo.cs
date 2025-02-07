e6e4b7fe0f9bad03d13b2ebc825e5b20958a9a99
KricsDavid <david@kricsfalusi.hu>
2025-02-07 07:57:23 +0100

    gitignore frissites

--- BuliHub_Backend/Bulihub_Backend/obj/Debug/net8.0/Bulihub_Backend.AssemblyInfo.cs
+++ BuliHub_Backend/Bulihub_Backend/obj/Debug/net8.0/Bulihub_Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bulihub_Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df117236763cb93628ddd789b8f336d006176da0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ab31ac2dbaacaf6f719e4f0e4e82fcbcab9a112")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bulihub_Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bulihub_Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
