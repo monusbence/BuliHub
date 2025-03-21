562e113c62435d1622b1c5224aaeb0e72aaee0d6
Takacs-Balazs <t.balazs521@gmail.com>
2025-03-21 14:34:09 +0100

    mm

--- WPF/AdminApp/AdminApp/MainWindow.xaml.cs
+++ WPF/AdminApp/AdminApp/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace AdminApp
 {
@@ -7,6 +10,11 @@ namespace AdminApp
         public MainWindow()
         {
             InitializeComponent();
+            var imageSource = new BitmapImage();
+            imageSource.BeginInit();
+            imageSource.StreamSource = new FileStream("AppLogo(png).png", FileMode.Open, FileAccess.Read);
+            imageSource.EndInit();
+            imgLogo.Source =  imageSource;
         }
 
         private void RegisterButton_Click(object sender, RoutedEventArgs e)
